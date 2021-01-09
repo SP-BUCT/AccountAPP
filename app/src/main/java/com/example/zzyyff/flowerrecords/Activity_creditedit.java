1f00379b015ffde2c60ae9959f22e2654a212df0
metaSSK <mateworld@outlook.com>
2021-01-09 15:43:31 +0800

    fix credit-edit bug & Optimize the layout

--- app/src/main/java/com/example/zzyyff/flowerrecords/Activity_creditedit.java
+++ app/src/main/java/com/example/zzyyff/flowerrecords/Activity_creditedit.java
@@ -1,7 +1,6 @@
 package com.example.zzyyff.flowerrecords;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
@@ -12,13 +11,18 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Activity_creditedit extends AppCompatActivity {
     //接受intent数据
     String addoredit;
     String ctype;
+    String cname;
+    String cbalance;
+    String cremark;
+//    String select;
 
-
+    //账户编辑页面
     ImageView back;
     ImageView ok;
     EditText type;
@@ -26,8 +30,13 @@ public class Activity_creditedit extends AppCompatActivity {
     EditText balance;
     EditText remark;
 
+    //账户选择页面
     AlertDialog creditTypeSelect;
     View popCreditType;
+    LinearLayout type_cash;
+    LinearLayout type_saving;
+    LinearLayout type_credit;//指账户类别中的信用卡
+    LinearLayout type_online;//指账户类别中的网络支付账户
 
     final ContentValues values = new ContentValues();
 
@@ -58,24 +67,72 @@ public class Activity_creditedit extends AppCompatActivity {
         balance = (EditText) findViewById(R.id.edit_balance);
         remark = (EditText) findViewById(R.id.edit_remark);
 
-        type.setText(ctype.toCharArray(),0,ctype.length());
-
+        initAlertDialog();
         initClickListener();
+
 }
 
+
+    private void initAlertDialog() {
+        creditTypeSelect = new AlertDialog.Builder(Activity_creditedit.this).create();
+        popCreditType = LayoutInflater.from(Activity_creditedit.this).inflate(R.layout.pop_credittype,null);
+        creditTypeSelect.setView(popCreditType);
+        creditTypeSelect.setCanceledOnTouchOutside(true);
+
+        type_cash = popCreditType.findViewById(R.id.type_cash);
+        type_saving = popCreditType.findViewById(R.id.type_saving);
+        type_credit = popCreditType.findViewById(R.id.type_credit);
+        type_online = popCreditType.findViewById(R.id.type_online);
+
+        type.setText(ctype.toCharArray(),0,ctype.length());
+
+    }
     private void initClickListener() {
         type.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                creditTypeSelect = new AlertDialog.Builder(Activity_creditedit.this).create();
-                popCreditType = LayoutInflater.from(Activity_creditedit.this).inflate(R.layout.pop_credittype,null);
-                creditTypeSelect.setView(popCreditType);
+                initAlertDialog();
                 creditTypeSelect.show();
+
+                type_cash.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        type.setText("现金");
+                        creditTypeSelect.dismiss();
+                    }
+
+                });
+                type_saving.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        type.setText("储蓄卡");
+                        creditTypeSelect.dismiss();
+                    }
+                });
+                type_credit.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        type.setText("信用卡");
+                        creditTypeSelect.dismiss();
+                    }
+                });
+                type_online.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        type.setText("网络支付账户");
+                        creditTypeSelect.dismiss();
+                    }
+                });
+
             }
         });
 
+        final Intent intent = getIntent();
+        addoredit = intent.getStringExtra("addoredit");
+        ctype = intent.getStringExtra("ctype");
 
+//        type.setText(select);
 
         switch (addoredit){
             case "add":
@@ -83,9 +140,9 @@ public class Activity_creditedit extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
 
-                        String cname = name.getText().toString();
-                        String cbalance = balance.getText().toString();
-                        String cremark = remark.getText().toString();
+                        cname = name.getText().toString();
+                        cbalance = balance.getText().toString();
+                        cremark = remark.getText().toString();
 
                         ContentValues values = new ContentValues();
                         values.put("type",ctype);
@@ -103,7 +160,48 @@ public class Activity_creditedit extends AppCompatActivity {
                 });
                 break;
             case "edit":
+                final int id = intent.getIntExtra("id",0);
+                cname = intent.getStringExtra("cname");
+                cbalance = intent.getStringExtra("cbalance");
+                cremark = intent.getStringExtra("cremark");
 
+                cbalance = cbalance.replace("￥","");
+
+                name.setText(cname);
+                balance.setText(cbalance);
+                remark.setText(cremark);
+
+                ok.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        ctype = type.getText().toString();
+                        cname = name.getText().toString();
+                        cbalance = balance.getText().toString();
+                        cremark = remark.getText().toString();
+
+                        ok.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                                ContentValues values = new ContentValues();
+                                values.put("type",ctype);
+                                values.put("name",cname);
+                                values.put("balance",cbalance);
+                                values.put("remarks",cremark);
+
+
+                                db.update("credit",values,"id=?",new String[]{String.valueOf(id)});
+                                Intent intent1 = new Intent();
+                                intent1.putExtra("position_return",2);
+                                setResult(RESULT_OK,intent1);
+                                finish();
+                            }
+                        });
+
+
+                    }
+                });
                 break;
 
         }
